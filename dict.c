812771b4e2c64dfdbb1a37e666cf64fc79d080fc
Chad Granum <exodist7@gmail.com>
2015-02-22 22:14:06 -0800

    Work on node allocation

--- dict.c
+++ dict.c
@@ -1,71 +1,267 @@
-#include "Dict.h";
+#include "dict.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
-typedef struct node   node;
-typedef struct vnode  vnode;
-typedef struct store  store;
-typedef struct sstore sstore;
-typedef struct vstore vstore;
+typedef struct node  node;
+typedef struct store store;
 
 struct node {
-    void     *key;
-    uint64_t *hash;
-    node     *left;
-    node     *right;
-};
+    size_t hash;
 
-struct vnode {
-    node   node;
-    void  *val;
+    void *key;
+    void *val;
+
+    node *left;
+    node *right;
 };
 
 struct store {
-    size_t count;
-    uint64_t id;
+    size_t free_count;
+
+    store *next;
     node  *free;
-};
 
-struct sstore {
-    store   store;
-    node   *nodes;
-    sstore *child;
-};
+    store *left;
+    store *right;
 
-struct vstore {
-    store   store;
-    vnode  *vnodes;
-    vstore *child;
+    size_t index;
+    node   nodes[]; // root ? 6 x base : 2 x base
 };
 
 struct dict {
-    uint64_t seed;
-    vstore base;
+    size_t base;
+    store *store;
+    store root;
+    size_t free;
 };
 
-struct set {
-    uint64_t seed;
-    sstore base;
-};
+// store utils
+inline size_t store_size(size_t base, int root);
+inline int address_in_store(uintptr_t addr, uintptr_t s, size_t base);
+node *grab_next(store *s, size_t base);
+node *grab_free(store *s);
+store *find_store(dict *d, void *want, store **parent, int *dir);
 
+// Node allocation
+node *alloc_node(dict *d, void *key, node *root, node *parent);
+node *unalloc_node(dict *d, node *n);
 
-// Free
+// Node locations
+node *find_root_node(store *root_store, uint64_t hash);
+node *find_node(node *root, uint64_t hash, void *key, node **parent);
+
+// Node management
+void *update_node(node *n, void *old, void *new); // Atomic swap, allow NULL
+void *insert_node(node *nearest, node *new);      // Atomic insert, loop with a search+update on nearest
+void *remove_node(node *n, node *parent);         // Note: Assert key set to NULL already
+
+//#########################################################################
+
+inline size_t store_size(size_t base, int root) {
+    size_t out   = sizeof(store);
+    size_t nsize = sizeof(node);
+    return (out - nsize) + (base * nsize * (root ? 6 : 2));
+}
+
+inline int address_in_store(uintptr_t addr, uintptr_t s, size_t base) {
+    uintptr_t end = s + store_size(base, 0);
+    if (addr > end || addr < s) return 0;
+    return 1;
+}
+
+node *grab_free(store *s) {
+    while(1) {
+        node *new = s->free;
+        if (!new) return NULL;
+
+        s->free = new->val; // TODO atomic || try again
+        s->free_count--;      // TODO atomic
+
+        return new;
+    }
+}
+
+node *grab_next(store *s, size_t base) {
+    size_t s_size = 2 * base;
+    size_t idx = s->index; // atomic read
+
+    if (idx + 1 >= s_size) return NULL;
+
+    s->index = idx + 1; // atomic compare and swap || continue
+
+    return s->nodes + idx;
+}
+
+store *find_store(dict *d, void *want, store **parent, int *dir) {
+    if (parent) *parent = NULL;
+    if (dir) *dir = 0;
+    store *f = &(d->root);
+
+    while (f && !address_in_store((uintptr_t)want, (uintptr_t)f, d->base)) {
+        if (parent) *parent = f;
+
+        if ((uintptr_t)want < (uintptr_t)f) {
+            if (dir) *dir = -1;
+            f = f->left;
+        }
+        else {
+            if (dir) *dir = 1;
+            f = f->right;
+        }
+    }
+
+    return f;
+}
+
+node *alloc_node(dict *d, void *key, node *root, node *parent) {
+    assert(root);
+    if (!parent) parent = root;
+    node *new = NULL;
+
+    while(1) {
+        // The root node, which is always allocated, is available
+        if (!root->key) {
+            if (root->key = key) { // TODO Atomic
+                return root;
+            }
+            continue;
+        }
+
+        // find the correct free store
+        store *f = find_store(d, parent, NULL, NULL);
+
+        // Grab one from the correct store
+        if (f) {
+            new = grab_free(f);
+            if (new) break;
+        }
+
+        // Grab one from any free list
+        f = &(d->root);
+        while (f) {
+            new = grab_free(f);
+            if (new) break;
+            f = f->next; // atomic read
+        }
+
+        // Grab next one from current store
+        new = grab_next(d->store, d->base);
+        if (new) break;
+
+        // Create a new store
+        store *old = d->store; // atomic read
+        size_t s_size = store_size(d->base, 0);
+
+        // Only place where allocating a node can truly fail, out of memory.
+        store *s = malloc(s_size);
+        if (!s) return NULL;
+
+        memset(s, 0, s_size);
+        new = grab_next(s, d->base);
+        s->next = old; // atomic set
+        if (!(d->store = s)) { // atomic cmp+swap
+            // Someone beat us to it.
+            new = NULL;
+            free(s);
+            continue;
+        }
+
+        // Insert new store into the tree
+        while(1) {
+            store *pstore = NULL;
+            int dir;
+            store *f = find_store(d, s, &pstore, &dir);
+            assert(!f);
+            assert(pstore);
+            assert(dir == 1 || dir == -1);
+            if (dir == -1) {
+                pstore->left = s; // atomic compare && break
+            }
+            else {
+                pstore->right = s; // atomic compare && break;
+            }
+        }
+    }
+
+    new->key = key; // TODO atomic
+    return new;
+}
+
+node *unalloc_node(dict *d, node *n);
+
+// Node locations
+node *find_root_node(store *root_store, uint64_t hash);
+node *find_node(node *root, uint64_t hash, void *key, node **parent);
+
+// Node management
+void *update_node(node *n, void *old, void *new); // Atomic swap, allow NULL
+void *insert_node(node *nearest, node *new);      // Atomic insert, loop with a search+update on nearest
+void *remove_node(node *n, node *parent);         // Note: Assert key set to NULL already
+
+dict *dict_create(size_t base) {
+    size_t sts = store_size(base, 1) - sizeof(store);
+    size_t dsize = sizeof(dict) + sts;
+    dict *dict = malloc(dsize);
+    if (!dict) return NULL;
+    memset(dict, 0, dsize);
+
+    dict->base  = base;
+    dict->store = &(dict->root);
+
+    return dict;
+}
+
 void dict_free(dict *d);
-void  set_free(set  *s);
 
-// Create a new one
-dict *dict_create(size_t base, uint64_t seed);
-set   *set_create(size_t base, uint64_t seed);
+void *dict_insert(dict *d, uint64_t hash, void *key, void *val) {
+    node *new = NULL;
 
-/*
- * ALL operations are atomic unless otherwise noted, also completely thread
- * safe.
- */
+    node *root   = find_root_node(&(d->root), hash);
+    node *parent = NULL;
 
-/* Insert a new node, return value of node
- * If node already exists value of the node is returned
- * If node does not exist create it and return the newly set value (same as val)
- * If node cannot be created return NULL
- */
-void *dict_insert(dict *d, uint64_t hash, void *key, void *val);
+    node *existing = NULL;
+    void *ekey     = NULL;
+
+    while(1) {
+        existing = find_node(root, hash, key, &parent);
+        ekey = existing->key;
+
+        // If we got an existing one transaction fails, unless it is in the process of being deleted
+        if (existing) {
+            if (ekey) return ekey;
+            continue; // Deletion in progress.
+        }
+
+        new = alloc_node(d, key, root, parent); // Optimistic
+        if (!new) return NULL;
+
+        //new->key = key; // already done
+        new->val   = val;
+        new->hash  = hash;
+        new->left  = NULL;
+        new->right = NULL;
+
+        // Add it to parent
+        if (hash <= parent->hash) {
+            parent->left = new; // TODO atomic!
+        }
+        else {
+            parent->right = new; // TODO atomic!
+        }
+
+        // Try again :-(
+        if (new == root) {
+            new = NULL;
+        }
+        else {
+            unalloc_node(d, new);
+        }
+    }
+
+    // Success
+    return val;
+}
 
 /* Update an existing node, return value of node
  * If node does not exist, return NULL
@@ -87,27 +283,7 @@ void *dict_check(dict *d, uint64_t hash, void *key);
 
 /* Set the value of a node, ignoring current value, returns value of node
  * If node cannot be inserted, return NULL (out of memory)
- * If value cannot be changed (can this happen?) return old value
+ * If value cannot be changed return old value
  * Return the newly set value.
  */
 void *dict_set(dict *d, uint64_t hash, void *key, void *newval);
-
-/* Insert a key into the set
- * return 0  (false) if it fails
- * return 1  (true)  if it succeeds
- * return -1 (true)  if it is already set
- */
-int set_insert(set *s, uint64_t hash, void *key);
-
-/* Remove a key from the set
- * return 1  (true)  if the key was present and removed
- * return -1 (true)  if the key was not present
- * return 0  (false) if the key could not be removed
- */
-int set_remove(set *s, uint64_t hash, void *key);
-
-/* Check if a key is in a set
- * return 1 (true)  if the key is present
- * return 0 (false) if the key is not present
- */
-int set_check(set *s, uint64_t hash, void *key);
--- dict.h
+++ dict.h
@@ -1,16 +1,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct set  set;
 typedef struct dict dict;
 
-// Free
-void dict_free(dict *d);
-void  set_free(set  *s);
-
-// Create a new one
-dict *dict_create(size_t base, uint64_t seed);
-set   *set_create(size_t base, uint64_t seed);
+dict *dict_create(size_t base);
+void  dict_free(dict *d);
 
 /*
  * ALL operations are atomic unless otherwise noted, also completely thread
@@ -44,27 +38,7 @@ void *dict_check(dict *d, uint64_t hash, void *key);
 
 /* Set the value of a node, ignoring current value, returns value of node
  * If node cannot be inserted, return NULL (out of memory)
- * If value cannot be changed (can this happen?) return old value
+ * If value cannot be changed return old value
  * Return the newly set value.
  */
 void *dict_set(dict *d, uint64_t hash, void *key, void *newval);
-
-/* Insert a key into the set
- * return 0  (false) if it fails
- * return 1  (true)  if it succeeds
- * return -1 (true)  if it is already set
- */
-int set_insert(set *s, uint64_t hash, void *key);
-
-/* Remove a key from the set
- * return 1  (true)  if the key was present and removed
- * return -1 (true)  if the key was not present
- * return 0  (false) if the key could not be removed
- */
-int set_remove(set *s, uint64_t hash, void *key);
-
-/* Check if a key is in a set
- * return 1 (true)  if the key is present
- * return 0 (false) if the key is not present
- */
-int set_check(set *s, uint64_t hash, void *key);
